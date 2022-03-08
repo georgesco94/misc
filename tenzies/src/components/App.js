ce3d94cd0608dfa6323fb35c9cc1357167935a85
George Najm <georgenajm@Georges-MacBook-Pro-2.local>
2022-03-08 16:48:44 -0500

    spaced dice

--- tenzies/src/components/App.js
+++ tenzies/src/components/App.js
@@ -2,21 +2,32 @@ import React from 'react';
 import '../styles/App.css';
 import Die from './Die.js';
 
+
 export default function App() {
+  function allNewDice() {
+    const arrOfNums = [];
+    for (var i = 0; i < 10; i++) {
+      arrOfNums.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return arrOfNums;
+  }
+
+  const [dice, setDice] = React.useState(allNewDice());
+
+  const dieElements = dice.map((die, idx) => {
+    return <Die dieNumber={die}/>;
+  });
+
+  function handleRoll() {
+    setDice(allNewDice());
+  }
+
   return(
     <main className="main">
       <div className="dice-container">
-        <Die dieNumber={1} />
-        <Die dieNumber={1} />
-        <Die dieNumber={5} />
-        <Die dieNumber={2} />
-        <Die dieNumber={1} />
-        <Die dieNumber={4} />
-        <Die dieNumber={2} />
-        <Die dieNumber={3} />
-        <Die dieNumber={4} />
-        <Die dieNumber={6} />
+        {dieElements}
       </div>
+      <button className="die-roll-button" onClick={handleRoll}>Roll</button>
     </main>
   );
 }
