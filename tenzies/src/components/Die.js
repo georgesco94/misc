3c7260d0d9310a57007817bd45580b99901376d0
George Najm <georgenajm28@yahoo.com>
2022-03-08 17:06:10 -0500

    held styles

--- tenzies/src/components/Die.js
+++ tenzies/src/components/Die.js
@@ -3,7 +3,7 @@ import '../styles/Die.css';
 
 export default function Die(props) {
   return(
-    <div className="die-face">
+    <div className={"die-face " + (props.isHeld ? "held" : "")}>
       <h2 className="die-num">{props.dieNumber}</h2>
     </div>
   );
