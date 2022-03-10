d16853e56c3a989e66c180517739ceb623aca586
George Najm <georgenajm28@yahoo.com>
2022-03-10 00:27:07 -0500

    added some rewritten components

--- thinking-in-react-demo/src/App.js
+++ thinking-in-react-demo/src/App.js
@@ -4,14 +4,47 @@ import data from './data.js';
 import Filters from './Filters.js';
 import Table from './Table.js';
 
+
+// export default function App() {
+//   console.log('functional');
+//   const [showInStock, setShowInStock] = React.useState(false);
+//   const [filterText, setFilterText] = React.useState('');
+//   const [inventory, setInventory] = React.useState([]);
+//
+//   React.useEffect(() => {
+//     setInventory(data)
+//   }, []);
+//
+//   return (
+//     <div className="inventory-app">
+//       <Filters
+//         showInStock={showInStock}
+//         filterText={filterText}
+//         toggleShowInStock={() => setShowInStock(prev => !prev)}
+//         updateFilterText={(e) => setFilterText(e.target.value)}
+//       />
+//       <Table
+//         inventory={inventory}
+//         showInStock={showInStock}
+//         filterText={filterText}
+//       />
+//     </div>
+//   )
+// }
+
 export default class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      inventory: [],
-      showInStock: false,
-      filterText: '',
-    }
+  // constructor(props){
+  //   super(props)
+  //   this.state = {
+  //     inventory: [],
+  //     showInStock: false,
+  //     filterText: '',
+  //   }
+  // }
+  state = {
+    inventory: [],
+    showInStock: false,
+    filterText: '',
   }
 
   componentDidMount() {
